c212836a999df34d72308edc7493ef23936c29f4
Shashank Taliwal <shashanktaliwal07@gmail.com>
2021-07-25 19:22:31 +0530

    Everything good to go

--- models/shortURL.js
+++ models/shortURL.js
@@ -1,23 +1,21 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
+const shortId = require('shortid')
 
-//ShortID is a NPM package for generating random short values cooresponding to a long URL
-const shortID = require("shortid");
+const shortUrlSchema = new mongoose.Schema({
+  full: {
+    type: String,
+    required: true
+  },
+  short: {
+    type: String,
+    required: true,
+    default: shortId.generate
+  },
+  clicks: {
+    type: Number,
+    required: true,
+    default: 0
+  }
+})
 
-const shortURLschema= new mongoose.Schema({
-    big:{
-        type:String,
-        required: true
-    },
-    small:{
-        type:String,
-        required:true,
-        default: shortID.generate //This will generate a unique shortID everytime it gets called
-    },
-    clicks:{
-        type:Number,
-        required:true,
-        default:0
-    }
-});
-
-module.exports=mongoose.model('shortURL',shortURLschema);
+module.exports = mongoose.model('ShortUrl', shortUrlSchema)
